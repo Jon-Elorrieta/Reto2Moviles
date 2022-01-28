3ddda2c0a308bb04598a590aff74f2cab1f945e5
Jon-Elorrieta <92148600+Jon-Elorrieta@users.noreply.github.com>
2022-01-28 10:15:11 +0100

    Menu camara new

--- Reto22/app/src/main/java/com/example/reto2moviles/CamaraActivity.java
+++ Reto22/app/src/main/java/com/example/reto2moviles/CamaraActivity.java
@@ -40,84 +40,50 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class CamaraActivity extends AppCompatActivity {
-Button sacarf;
-Button guardarf;
+Button opciones;
+
 ImageView Vistaimg;
 String rutaImagen;
     Uri imageUri;
 
-    int PICK_IMAGE = 100;
-    //int SELECT_PICTURE = 200;
+
     static final int CAPTURA_IMAGEN= 1;
-     static final int CAPTURA_IMAGEN_Tamaño_REAL= 2;
 
 
-    private final int MY_PERMISSIONS = 100;
-    private final int PHOTO_CODE = 200;
-    private final int SELECT_PICTURE = 300;
-
-    private ImageView mSetImage;
-    private Button mOptionButton;
-    private RelativeLayout mRlView;
-
-    private String mPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camara);
-        sacarf = findViewById(R.id.btn_sacarfoto);
-        guardarf = findViewById(R.id.btn_sacarfoto);
+        opciones = findViewById(R.id.btn_sacarfoto2);
+
         Vistaimg= findViewById(R.id.imgView);
 
 
     }
 
 
-
-    public void Sacarfoto(View view) {
-
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        //Tenemos camara?
-        //if (intent.resolveActivity(getPackageManager()) != null) {
-            //startActivityForResult(intent, CAPTURA_IMAGEN);
-
-            File imagenArchivo = null;
-
-            try {
-                imagenArchivo = crearImagenn();
-                if (imagenArchivo != null) {
-                    Uri photoUri = FileProvider.getUriForFile(this, "com.example.reto2moviles.fileprovider", imagenArchivo);
-                    intent.putExtra(MediaStore.EXTRA_OUTPUT,photoUri);
-                    startActivityForResult(intent,CAPTURA_IMAGEN);
-                }
-            } catch (IOException ex) {
-                Log.e("Error", ex.toString());
-            }
+    public void opciones(View view){
+        mostrarDialogOpciones();
+    }
 
 
-       }
+
 
 
     private void mostrarDialogOpciones() {
-        final CharSequence[] opciones={"Tomar Foto","Elegir de Galeria","Subir Foto","Cancelar"};
+        final CharSequence[] opciones={"Elegir de Galeria","Subir Foto","Cancelar"};
         final AlertDialog.Builder builder=new AlertDialog.Builder(CamaraActivity.this);
         builder.setTitle("Elige una Opción");
         builder.setItems(opciones, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                if (opciones[i].equals("Tomar Foto")){
-                    abriCamara();
-                }else if(opciones[i].equals("Subir Foto")) {
+               if(opciones[i].equals("Subir Foto")) {
 
                 }
                 else{
                     if (opciones[i].equals("Elegir de Galeria")){
-                        Intent intent=new Intent(Intent.ACTION_PICK,
-                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                        intent.setType("image/");
-                        startActivityForResult(intent.createChooser(intent,"Seleccione"),COD_SELECCIONA);
+                        imageChooser();
                     }else{
                         dialogInterface.dismiss();
                     }
@@ -127,19 +93,29 @@ String rutaImagen;
         builder.show();
     }
 
+    public void Sacarfoto(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-/*
-    private void galleryAddPic(){
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File f = new File(path);
-        Uri contentUri = Uri.fromFile(f);
-        mediaScanIntent.setData(contentUri);
-        this.sendBroadcast(mediaScanIntent);
+        //Tenemos camara?
+        //if (intent.resolveActivity(getPackageManager()) != null) {
+        //startActivityForResult(intent, CAPTURA_IMAGEN);
+
+        File imagenArchivo = null;
+
+        try {
+            imagenArchivo = crearImagenn();
+            if (imagenArchivo != null) {
+                Uri photoUri = FileProvider.getUriForFile(this, "com.example.reto2moviles.fileprovider", imagenArchivo);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,photoUri);
+                startActivityForResult(intent,10);
+            }
+        } catch (IOException ex) {
+            Log.e("Error", ex.toString());
+        }
+
+
     }
 
-
-
- */
 
 
 
@@ -204,16 +180,6 @@ String rutaImagen;
     }
 
 
-
-
-    public void SubirFoto(View view){
-
-
-    }
-
-    public void BuscarFoto(View view){
-      imageChooser();
-    }
 
     public void volver(View view){
        onBackPressed();
